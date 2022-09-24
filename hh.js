d30869af19f68d2f9fa54f08e2e4e00d2b539375
pravin016 <sharmapravin016@gmail.com>
2022-09-24 18:24:39 +0530

    initial commit

---
+++ hh.js
@@ -0,0 +1,146 @@
+//  sjmdnsdnmsdmdsm 
+// variables
+/*var n1 =45;
+var n3=76;
+console.log(n1+n3);
+// strings
+var str ="hjsjhsjhffs";
+var str1="ffsdfdfs";
+//objects
+var marks ={
+    ravi :44,
+    ram :42,
+    raju:66,
+}
+
+console.log(marks);
+var a = true;
+var b= false;
+console.log(a,b);
+var und= null;
+console.log(und);
+
+var arr =[1,2,"APPLE",5]
+var a =27;
+var b=8;
+console.log("add", a+b);
+console.log("sub", a-b);
+console.log("multiply", a*b);
+console.log("div", a/b);
+console.log(true||true);
+console.log(true||false);
+console.log(false||true);
+console.log(false||false);
+
+function avg (a,b){
+    c=(a+b)/2;
+    return c;
+}
+c1=avg(4,5);
+c2=avg(5,2);
+console.log(c1,c2);
+if(a>18){
+    console.log('adult');
+}
+else if(a>40){
+    console.log('working');
+}
+else if(a>60){
+    console.log('budhaa');
+}
+*/
+
+
+/*var a = [1, 2, 3, 4, 5,6,7];
+for (var i = 0; i < a.length; i++) {
+
+{
+    if(i==2){
+     break;
+}  
+    console.log(arr[i])
+}
+}
+*/
+
+/*
+arr.forEach(function(element)){
+    console.log(element);
+}
+*/
+/*
+let j=0;
+do{
+    if(j==2){ 
+    }
+    console.log(a[j]);
+    j++;
+}
+while(j<a.length);
+*/    
+/*
+let my =[1,2,"ram","shyam",true];
+//console.log(my.length);
+const m = my.unshift("hi")
+//console.log(m)
+let Str ="java script is is is  interesting";
+//console.log(Str.length)
+//console.log(Str.lastIndexOf("is"))
+d=Str.replace("java","python");
+//console.log(d,Str)
+let mydate = new Date();
+//console.log(mydate);
+// dom manipulation  
+let elem = document.getElementById('click');
+//console.log(elem);
+let elemclass=document.getElementsByClassName("container") 
+// elemclass[0].style.background ="yellow";
+elemclass[0].classList.add("bg-primary");
+elemclass[0].classList.add("text-sucess");
+//console.log(elem.innerHTML);
+//console.log(elem.innerText);
+tn =document.getElementsByTagName('div')
+console.log(tn)
+createdElement=document.createElement('p');
+createElement.innerText='this is a created element';
+createdElement2=document.createElement('b');
+tn[0].appendChild(createdElement);
+createElement2.innerText='this is a created bold';
+tn[0].replaceChild(createdElement2,createdElement);
+
+sel =document.querySelector('.container')
+console.log(sel)
+sel =document.querySelectorAll('.container')
+console.log(sel)
+
+function clicked(){
+    console.log('the button was clicked')
+}
+window.onload = function(){
+    console.log('document was loaded')
+}
+click.addEventListner('click',function()){
+console.log('click hua')
+ }
+ */
+summ =(a,b)=>{
+    return a+b;
+}
+multi =(m,n)=>{
+    return m*n;
+}
+//log =()=>{
+
+    //document.querySelectorAll('.container')[1].innerHTML=<b>we have clicked</b>
+
+    console.log("i am lion")
+}
+//clr =setInterval(log,200);
+  obj = {name :"harry", length: 1}
+  jso = JSON.stringify(obj);
+ console.log(typeof jso)
+ console.log(jso)  
+
+
+
+
